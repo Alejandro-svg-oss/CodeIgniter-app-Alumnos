f3e44c55ba906ccf075a8bf46d8ad56ddbc92168
Alejandro <ia.ch100423@ufg.edu.sv>
2026-02-22 14:01:18 -0600

    busqueda por carrera

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -13,3 +13,7 @@ $routes->post('alumnos/store', [AlumnosController::class, 'store']);
 $routes->get('alumnos/edit/(:num)', [AlumnosController::class, 'edit/$1']);
 $routes->post('alumnos/update/(:num)', [AlumnosController::class, 'update/$1']);
 $routes->post('alumnos/delete/(:num)', [AlumnosController::class, 'delete/$1']);
+
+// Rutas para busqueda de alumnos por carrera
+$routes->get('alumnos_carrera', 'Alumnosxcarrera::index');
+$routes->post('alumnos_carrera/filtrar', 'Alumnosxcarrera::filtrar');
